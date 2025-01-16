211b6c634d26de6bd09b2d71571890e57a4c5b69
Sahil_Wazade <swazade@provepartners.com>
2025-01-16 16:37:17 +0530

    created_clean_code_infracture

---
+++ EmailService.Application/DependancyResolver/DependancyResolverExtention.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService.Application.DependancyResolver
+{
+    public static class DependancyResolverExtention
+    {
+        public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
